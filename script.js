5ea5f37026a889b7eca0960295994a45d7fe24e6
Anastasiia <middlenast@gmail.com>
2022-12-29 13:44:07 +0300

    feat: add popup window

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/*
+Read json data from the file with information about pets
+*/
 let pet = [];
 fetch('./pets.json')
     .then(response => {
@@ -8,7 +11,9 @@ fetch('./pets.json')
         console.log(data);
     });
 
-
+/*
+Create a burger menu
+*/
 const burger = document.querySelector(".burger");
 burger.addEventListener('click',() => {
     let x = document.getElementById("links");
@@ -20,4 +25,38 @@ burger.addEventListener('click',() => {
         burger.src = "./assets/icons/Burger-vert.svg";
     }
 });
+
+/*
+Create a pop-up window with pet descriptions
+*/
+let popupBg = document.querySelector('.popup__bg');
+let popup = document.querySelector('.popup');
+let openPopupButtons = document.querySelectorAll('.open-popup');
+let closePopupButton = document.querySelector('.close-popup');
+
+// Open pop-up
+openPopupButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        popupBg.classList.add('active');
+        popup.classList.add('active');
+        let id = e.target.id;
+        for (let i=0; i < pet.length; i++) {
+            if (pet[i]['name'] === id) {
+                let titleDescription = document.createElement('h5');
+                titleDescription.classList.add('active-h5');
+                titleDescription.innerHTML = id;
+                popup.appendChild(titleDescription);
+            }
+        }
+    })
+});
+
+// Close pop-up
+closePopupButton.addEventListener('click',() => {
+    popupBg.classList.remove('active');
+    popup.classList.remove('active');
+});
+
+
 

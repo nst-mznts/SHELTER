53a2bdeca1ff2f974f0a488876be9838867fe945
Anastasiia <middlenast@gmail.com>
2022-12-30 15:45:12 +0300

    feat: add slider to main page

--- script.js
+++ script.js
@@ -106,5 +106,50 @@ closePopupButton.addEventListener('click',() => {
     document.querySelector('.active-wrapper').remove();
 });
 
+/*
+Create slider
+*/
+const BTN_LEFT = document.querySelector("#button-left");
+const BTN_RIGHT = document.querySelector("#button-right");
+const CAROUSEL = document.querySelector("#carousel-cards");
+const ITEM_LEFT = document.querySelector("#item-left");
+const ITEM_RIGHT = document.querySelector("#item-right");
 
+const createCardTemplate = () => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    return card;
+}
+  
+const moveLeft = () => {
+    CAROUSEL.classList.add("transition-left");
+    BTN_LEFT.removeEventListener("click", moveLeft);
+    BTN_RIGHT.removeEventListener("click", moveRight);
+};
+  
+const moveRight = () => {
+    CAROUSEL.classList.add("transition-right");
+    BTN_LEFT.removeEventListener("click", moveLeft);
+    BTN_RIGHT.removeEventListener("click", moveRight);
+};
+  
+BTN_LEFT.addEventListener("click", moveLeft);
+BTN_RIGHT.addEventListener("click", moveRight);
 
+CAROUSEL.addEventListener("animationend", (animationEvent) => {
+    let changedItem;
+    if (animationEvent.animationName === "move-left") {
+      CAROUSEL.classList.remove("transition-left");
+      changedItem = ITEM_LEFT;
+      document.querySelector("#item-active").innerHTML = ITEM_LEFT.innerHTML;
+    } else {
+      CAROUSEL.classList.remove("transition-right");
+      changedItem = ITEM_RIGHT;
+      document.querySelector("#item-active").innerHTML = ITEM_RIGHT.innerHTML;
+    }
+    
+    changedItem.innerHTML = "";
+    for (let i = 0; i < 3; i++) {
+      const card = createCardTemplate();
+    }
+  })

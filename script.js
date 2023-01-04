94d0f6cdcc059437a5c5f0aff28a35a21ef9e291
Anastasiia <middlenast@gmail.com>
2023-01-04 14:43:43 +0300

    fix: change mobile menu

--- script.js
+++ script.js
@@ -14,16 +14,27 @@ fetch('./pets.json')
 /*
 Create a burger menu
 */
+const logo = document.querySelector('.logo');
+const logoMobile = document.querySelector('.logo-mobile');
 const burger = document.querySelector(".burger");
+const mobileBurger = document.querySelector(".mobile_burger");
+
+// Open menu
 burger.addEventListener('click',() => {
     let x = document.getElementById("links");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-        burger.src = "./assets/icons/Burger.svg";
-    } else {
-        x.style.display = "block";
-        burger.src = "./assets/icons/Burger-vert.svg";
-    }
+    x.style.display = "block";
+    logoMobile.style.display = 'flex';
+    logo.style.display = 'none';
+    burger.style.display = 'none';
+});
+
+// Close menu
+mobileBurger.addEventListener('click',() => {
+    let x = document.getElementById("links");
+    x.style.display = "none";
+    logoMobile.style.display = 'none';
+    logo.style.display = 'block';
+    burger.style.display = 'block';
 });
 
 /*
